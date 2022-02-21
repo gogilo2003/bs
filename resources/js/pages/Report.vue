cec1df94e5bf8c82ad1c7fee4385fd69b453785f
Ogilo G. D. Ouma <gogilo2003@hotmail.com>
2022-02-21 03:15:15 +0300

    Fixed report Download file naming typo

--- resources/js/pages/Report.vue
+++ resources/js/pages/Report.vue
@@ -113,10 +113,12 @@ export default {
             })
                 .then((response) => response.blob())
                 .then((blob) => {
+                    var date = new Date();
+                    var filename = `${date.getFullYear()}.${date.getMonth()}.${date.getDate()}-${type}.pdf`;
                     var url = window.URL.createObjectURL(blob);
                     var a = document.createElement("a");
                     a.href = url;
-                    a.download = "$type.pdf";
+                    a.download = filename;
                     document.body.appendChild(a); // we need to append the element to the dom -> otherwise it will not work in firefox
                     a.click();
                     a.remove(); //afterwards we remove the element again
